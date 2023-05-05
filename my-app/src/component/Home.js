67d89f93cf5ff11fc48af50725fe5c0ac483312d
bonnkekimani <bonnke.kimani@student.moringaschool.com>
2023-05-05 09:29:22 +0300

    Before Deployment

--- my-app/src/component/Home.js
+++ my-app/src/component/Home.js
@@ -9,11 +9,16 @@ function Home() {
   return (
     <div>
         <div className='homepage'>  
-            <img src={handyman} className='handypic' alt='Page not found'/>     
+            <img src={handyman} className='handypic' alt='Page not found'/> 
+            <div className='HHtitle'>
+            <h1 style={{ fontSize: 70 }}>𝙃𝘼𝙉𝘿𝙔𝙈𝘼𝙉 <br/>𝙎𝙏𝙊𝙋𝙎𝙃𝙊𝙋</h1>
+
+            </div> 
          </div>
          <About /> 
          <Services />
          <Reviews />
+       
 
          
     </div>
